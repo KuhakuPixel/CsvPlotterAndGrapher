187a8a31f78d058ec1c38d0adde578dd324fc3f5
KuhakuPixel <Nicholaspixels@gmail.com>
2021-03-30 16:43:07 +0700

    able to call get and put from script

---
+++ PythonBackend/calltest.py
@@ -0,0 +1,24 @@
+
+
+"""
+Testing Script
+
+testing from commandline:
+
+
+put:
+    curl http://localhost:5000/CsvReader/path1 -d "csvFilePath=C:/Users/Nicho/Desktop/Projects/CsvPlotterAndGrapher/csvTest.csv" -X PUT
+get:
+    curl http://localhost:5000/CsvReader/path1
+"""
+from requests import get,put
+import pandas as pd
+API_ENDPOINT='http://localhost:5000/CsvReader/path1'
+CSV_FILE_PATH='C:/Users/Nicho/Desktop/Projects/CsvPlotterAndGrapher/csvTest.csv'
+
+
+
+put(API_ENDPOINT, data={'csvFilePath': CSV_FILE_PATH})
+
+print(get('http://localhost:5000/CsvReader/path1').json())
+
--- PythonBackend/main.py
+++ PythonBackend/main.py
@@ -1,6 +1,8 @@
 from flask_restful import Resource, Api,reqparse
 from flask import Flask, request
 import pandas as pd
+import numpy as np
+import json
 app = Flask(__name__)
 api = Api(app)
 print("test")
@@ -17,15 +19,19 @@ class CsvReader(Resource):
 
     def get(self,pathID):
         #reading csv
-        #print(csvFilesPathDictionary)
-        #print(type(csvFilesPathDictionary))
-        #self.dataFrame = pd.read_csv(csvFilesPathDictionary)
-        #return {"Column Name":self.dataframe.columns}
-        return {pathID : csvFilesPathDictionary[pathID]}
+
+        print(csvFilesPathDictionary[pathID])
+        print(type(csvFilesPathDictionary[pathID])==str)
+        DataFrame=pd.read_csv(csvFilesPathDictionary[pathID])
+
+        columns=np.array(DataFrame.columns)
+        print(columns)
+        columnsJSON=json.dumps(columns.tolist())
+        return {pathID:columnsJSON}
 
 
     def put(self,pathID):
-        #reading csv
+        #reading argument and put it into a dictionary
         arguments=parser.parse_args()
         csvFilesPathDictionary[pathID]=arguments["csvFilePath"]
 
