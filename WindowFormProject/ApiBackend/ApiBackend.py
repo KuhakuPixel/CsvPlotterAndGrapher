d213515c264a18df085ab8ed4027050cddedcb27
KuhakuPixel <66545911+KuhakuPixel@users.noreply.github.com>
2021-04-02 20:48:18 +0700

    integrate python project with the solution

---
+++ WindowFormProject/ApiBackend/ApiBackend.py
@@ -0,0 +1,48 @@
+from flask_restful import Resource, Api,reqparse
+from flask import Flask, request
+import pandas as pd
+import numpy as np
+import json
+app = Flask(__name__)
+api = Api(app)
+print("test")
+
+
+
+#creating argument parser
+parser = reqparse.RequestParser()
+parser.add_argument('csvFilePath',type=str)
+csvFilesPathDictionary= {}
+class CsvReader(Resource):
+
+
+
+    def get(self,pathID):
+        #reading csv
+
+        print(csvFilesPathDictionary[pathID])
+        print(type(csvFilesPathDictionary[pathID])==str)
+        DataFrame=pd.read_csv(csvFilesPathDictionary[pathID])
+
+        columns=np.array(DataFrame.columns)
+        print(columns)
+        columnsJSON=json.dumps(columns.tolist())
+        return {pathID:columnsJSON}
+
+
+    def put(self,pathID):
+        #reading argument and put it into a dictionary
+        arguments=parser.parse_args()
+        csvFilesPathDictionary[pathID]=arguments["csvFilePath"]
+
+        return {pathID:csvFilesPathDictionary[pathID]}
+
+#api resource routing
+api.add_resource(CsvReader, '/CsvReader/<string:pathID>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
---
+++ WindowFormProject/ApiBackend/requestTest.py
@@ -0,0 +1,19 @@
+"""
+Testing Script
+testing from commandline:
+put:
+    curl http://localhost:5000/CsvReader/path1 -d "csvFilePath=C:/Users/Nicho/Desktop/Projects/CsvPlotterAndGrapher/csvTest.csv" -X PUT
+get:
+    curl http://localhost:5000/CsvReader/path1
+"""
+from flask import Flask, request
+from requests import get,put
+
+API_ENDPOINT='http://localhost:5000/CsvReader/path1'
+CSV_FILE_PATH='C:/Users/Nicho/Desktop/Projects/CsvPlotterAndGrapher/csvTest.csv'
+
+
+
+put(API_ENDPOINT, data={'csvFilePath': CSV_FILE_PATH})
+
+print(get('http://localhost:5000/CsvReader/path1').json())
