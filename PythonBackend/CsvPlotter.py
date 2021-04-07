491548e98f9946f8597c4e710a80da03673b9ec3
KuhakuPixel <Nicholaspixels@gmail.com>
2021-04-07 12:23:49 +0700

    add class and function to plot and reformat code
    the functions that are used to plot a column of a data frame  will return a numpy.ndarray

---
+++ PythonBackend/CsvPlotter.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+
+class CsvPlotter:
+    @staticmethod
+    def pyplotFigureToImageArray(figure):
+        """
+           turn pyplot.figure to an image in array format
+        """
+        canvas = FigureCanvas(figure)
+        # draw the canvas, cache the renderer
+        canvas.draw()
+
+        width, height = figure.get_size_inches() * figure.get_dpi()
+        img = np.frombuffer(canvas.tostring_rgb(), dtype='uint8').reshape(int(height), int(width), 3)
+        return img
+
+    @staticmethod
+    def histogram(x: np.ndarray) -> np.ndarray:
+        """
+           x is a numpy array
+           return a numpy array of the graph
+        """
+        fig, ax = plt.subplots()
+        ax.hist(x=x)
+        return CsvPlotter.pyplotFigureToImageArray(figure=fig)
--- PythonBackend/main.py
+++ PythonBackend/main.py
@@ -1,44 +1,81 @@
-from flask_restful import Resource, Api,reqparse
-from flask import Flask, request
-import pandas as pd
-import numpy as np
 import json
+
+import numpy as np
+import pandas as pd
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+from CsvPlotter import CsvPlotter
+
+"""
+containing the data such as dataframe,columns or ect
+"""
+
+
+class UserData:
+    dataFrame = pd.DataFrame(columns=['A', 'B', 'C'], index=range(5))
+    # <string:object>
+    csvFilesPathDictionary = {}
+    columnNametoBePlottedDictionary = {}
+
+
 app = Flask(__name__)
 api = Api(app)
-print("test")
+
+# creating argument parser
+
+csvReaderArgumentParser = reqparse.RequestParser()
+csvReaderArgumentParser.add_argument('csvFilePath', type=str)
 
 
-
-#creating argument parser
-parser = reqparse.RequestParser()
-parser.add_argument('csvFilePath',type=str)
-csvFilesPathDictionary= {}
 class CsvReader(Resource):
 
+    def get(self, path_id):
+        # reading csv
 
+        UserData.dataFrame = pd.read_csv(UserData.csvFilesPathDictionary[path_id])
 
-    def get(self,pathID):
-        #reading csv
-
-        print(csvFilesPathDictionary[pathID])
-        print(type(csvFilesPathDictionary[pathID])==str)
-        DataFrame=pd.read_csv(csvFilesPathDictionary[pathID])
-
-        columns=np.array(DataFrame.columns)
+        columns = np.array(UserData.dataFrame.columns)
         print(columns)
-        columnsJSON=json.dumps(columns.tolist())
-        return {pathID:columnsJSON}
+        columns_json = json.dumps(columns.tolist())
+        return {path_id: columns_json}
+
+    def put(self, path_id):
+        # reading argument and put it into a dictionary
+        arguments = csvReaderArgumentParser.parse_args()
+        UserData.csvFilesPathDictionary[path_id] = arguments["csvFilePath"]
+        return {path_id: UserData.csvFilesPathDictionary[path_id]}
 
 
-    def put(self,pathID):
-        #reading argument and put it into a dictionary
-        arguments=parser.parse_args()
-        csvFilesPathDictionary[pathID]=arguments["csvFilePath"]
+columnToHistogramArgumentParser = reqparse.RequestParser()
+columnToHistogramArgumentParser.add_argument('columnName', type=str)
 
-        return {pathID:csvFilesPathDictionary[pathID]}
 
-#api resource routing
-api.add_resource(CsvReader, '/CsvReader/<string:pathID>')
+class ColumnToHistogram(Resource):
+
+    def get(self, plot_id):
+        column_name = UserData.columnNametoBePlottedDictionary[plot_id]
+        x = UserData.dataFrame[column_name]
+        image_in_numpy_array = CsvPlotter.histogram(x=x)
+
+        # dimension of the array
+        img_shape = image_in_numpy_array.shape
+        img_shape_json = json.dumps(img_shape)
+
+        img_json = json.dumps(image_in_numpy_array.tolist())
+
+        return {plot_id: img_json, "img_shape": img_shape_json}
+
+    def put(self, plot_id):
+        # reading argument and put it into a dictionary
+        arguments = columnToHistogramArgumentParser.parse_args()
+        UserData.columnNametoBePlottedDictionary[plot_id] = arguments["columnName"]
+        return {plot_id: UserData.columnNametoBePlottedDictionary[plot_id]}
+
+
+# api resource routing
+api.add_resource(CsvReader, '/CsvReader/<string:path_id>')
+api.add_resource(ColumnToHistogram, '/Plotter/ColumnToHistogram/<string:plot_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
